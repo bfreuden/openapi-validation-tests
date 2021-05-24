85ee2ff642869de98c7604ad51133fccd71feebc
Bruno Freudensprung <bruno.freudensprung@kairntech.com>
2021-05-24 11:19:09 +0200

    added test with micronaut

---
+++ java-micronaut/src/main/java/bfreuden/PetController.java
@@ -0,0 +1,18 @@
+package bfreuden;
+
+import io.micronaut.http.annotation.*;
+
+@Controller("/shops/{shop}")
+public class PetController {
+
+    @Get(uri="/pets/{pet}", produces="application/json")
+    public String getPet(String pet) {
+        return "{ \"name\":" + pet + ", \"age\": 5 }";
+    }
+
+    @Post(uri="/pets/_search", produces="application/json")
+    public String searchPets() {
+        return "{\"hits\": [{ \"name\": \"wolfie\", \"age\": 5 }]}";
+    }
+
+}
